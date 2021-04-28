6114731ef60554f5667c904ece0edbf2ac2ecde0
wlewis0740 <wlewis0740@gmail.com>
2021-04-27 22:56:43 -0500

    rough web api implemented

---
+++ NetCheckAPI/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCheckAPI.Models
+{
+    public class Result
+    {
+        public string address { get; set; }
+        public string services { get; set; }
+        public string message { get;  set; }
+    }
+}
