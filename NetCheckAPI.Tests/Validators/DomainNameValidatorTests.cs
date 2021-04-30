e372ef4c9908a9c295bebc42961089baba597dd2
wlewis0740 <wlewis0740@gmail.com>
2021-04-30 10:58:40 -0500

    improve domain name validator to not allow partial domains

---
+++ NetCheckAPI.Tests/Validators/DomainNameValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetCheckAPI.Validators;
+
+namespace NetCheckAPI.Tests.Validators
+{
+    [TestClass]
+    public class DomainNameValidatorTests
+    {
+        [TestMethod]
+        public void TestValidate_valid() {
+            TestValidate("test.com", true);
+        }
+
+        [TestMethod]
+        public void TestValidate_invalid() {
+            TestValidate("test", false);
+        }
+
+        private void TestValidate(string networkId, bool isValid) {
+            var validator = new DomainNameValidator(null);
+
+            var response = validator.Validate(networkId);
+
+            Assert.AreEqual(isValid, response.isValid);
+        }
+    }
+}
--- NetCheckAPI/Validators/DomainNameValidator.cs
+++ NetCheckAPI/Validators/DomainNameValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;   
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace NetCheckAPI.Validators
@@ -10,7 +11,8 @@ namespace NetCheckAPI.Validators
         public DomainNameValidator(INetworkValidator validator) : base(validator) { }
 
         public override ValidationResponse Validate(string networkID) {
-            if (Uri.CheckHostName(networkID) == UriHostNameType.Dns) {
+            Regex rgx = new Regex(@"^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$");
+            if (rgx.IsMatch(networkID)) {
                 return new ValidationResponse { isValid = true, message = "Valid Domain Name" };
             } else {
                 return base.Validate(networkID);
